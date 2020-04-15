f4daf1df671264e2c8f04b8bf64213c14ebf87dc
Alexander Babel <alexandermbabel@protonmail.com>
2020-04-14 20:50:47 -0400

    redux store set up

---
+++ client/src/actions/types.js
@@ -0,0 +1,6 @@
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILED = 'LOGIN_FAILED';
+export const LOGOUT = 'LOGOUT';
+export const REGISTER_FAILED = 'REGISTER_FAILED';
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const AUTH_ERROR = 'AUTH_ERROR';
